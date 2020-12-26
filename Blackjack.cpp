ca354a02af9a6f01992fc7416ca50ed56614bc6c
ashayp22 <ashayparikh22@gmail.com>
2020-12-26 12:12:49 -0600

    added clases and objects

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -8,6 +8,13 @@
 #include <ctime>
 
 
+
+
+//constants 
+constexpr int maximumScore{ 21 }; //maximum score that a player can have
+constexpr int minimumDealerScore{ 17 }; //the minimum score a dealer will have
+constexpr int shuffleMinimum{ 46 }; //the minimum score a dealer will have
+
 //Enumerator for a Card's Rank
 enum class CardsRanks {
     RANK_2,
@@ -36,142 +43,100 @@ enum class CardsSuits {
     MAX_SUITS
 };
 
-//Structure for a single card
-struct Card {
-    CardsRanks rank{};
-    CardsSuits suit{};
-};
+class Card {
 
-//Structure for a player
-struct Player {
-    int score;
-};
+private:
+    CardsRanks m_rank{};
+    CardsSuits m_suit{};
 
-//Returns a card's suit and rank as a string with length 2
-std::string printCard(const Card &card) {
+public:
 
-    std::string code{ "" };
+    //Default Constructor
+    Card() = default;
 
-    switch (card.rank)
-    {
-    case CardsRanks::RANK_2:
-        code += '2';
-        break;
-    case CardsRanks::RANK_3:
-        code += '3';
-        break;
-    case CardsRanks::RANK_4:
-        code += '4';
-        break;
-    case CardsRanks::RANK_5:
-        code += '5';
-        break;
-    case CardsRanks::RANK_6:
-        code += '6';
-        break;
-    case CardsRanks::RANK_7:
-        code += '7';
-        break;
-    case CardsRanks::RANK_8:
-        code += '8';
-        break;
-    case CardsRanks::RANK_9:
-        code += '9';
-        break;
-    case CardsRanks::RANK_10:
-        code += 'T';
-        break;
-    case CardsRanks::RANK_JACK:
-        code += 'J';
-        break;
-    case CardsRanks::RANK_QUEEN:
-        code += 'Q';
-        break;
-    case CardsRanks::RANK_KING:
-        code += 'K';
-        break;
-    case CardsRanks::RANK_ACE:
-        code += 'A';
-        break;
-    default:
-        break;
+    Card(CardsRanks rank, CardsSuits suit) {
+        m_rank = rank;
+        m_suit = suit;
     }
 
-    switch (card.suit) {
-        case CardsSuits::SUIT_CLUBS:
-            code += "C";
+    //Returns a card's suit and rank as a string with length 2
+    std::string printCard() const {
+
+        std::string code{ "" };
+
+        switch (m_rank)
+        {
+        case CardsRanks::RANK_2:
+            code = "2 of";
             break;
-        case CardsSuits::SUIT_DIAMONDS:
-            code += "D";
+        case CardsRanks::RANK_3:
+            code = "3 of";
             break;
-        case CardsSuits::SUIT_HEARTS:
-            code += "H";
+        case CardsRanks::RANK_4:
+            code = "4 of";
             break;
-        case CardsSuits::SUIT_SPADES:
-            code += "S";
+        case CardsRanks::RANK_5:
+            code = "5 of";
+            break;
+        case CardsRanks::RANK_6:
+            code = "6 of";
+            break;
+        case CardsRanks::RANK_7:
+            code = "7 of";
+            break;
+        case CardsRanks::RANK_8:
+            code = "8 of";
+            break;
+        case CardsRanks::RANK_9:
+            code = "9 of";
+            break;
+        case CardsRanks::RANK_10:
+            code = "10 of";
+            break;
+        case CardsRanks::RANK_JACK:
+            code = "Jack of";
+            break;
+        case CardsRanks::RANK_QUEEN:
+            code = "Queen of";
+            break;
+        case CardsRanks::RANK_KING:
+            code = "King of";
+            break;
+        case CardsRanks::RANK_ACE:
+            code = "Ace of";
             break;
         default:
             break;
-    }
-
-    return code;
-}
-
-// aliases that will be used throughout the program
-using deck_type = std::array<Card, 52>;
-using index_type = deck_type::size_type;
-
-//constants 
-constexpr int maximumScore{ 21 }; //maximum score that a player can have
-constexpr int minimumDealerScore{ 17 }; //the minimum score a dealer will have
-constexpr int shuffleMinimum{ 46 }; //the minimum score a dealer will have
-
-//creates and returns a deck of 52 cards
-deck_type createDeck() {
-
-    deck_type deck{};
-
-    index_type nextIndex{ 0 };
-
-    //convert enum into int
-    auto suits{ static_cast<int>(CardsSuits::MAX_SUITS) };
-    auto ranks{ static_cast<int>(CardsRanks::MAX_RANKS) };
-    
-    for (int suit = 0; suit < suits; suit++) {
-        for (int rank = 0; rank < ranks; rank++) {
-            deck[nextIndex].suit = static_cast<CardsSuits>(suit);
-            deck[nextIndex].rank = static_cast<CardsRanks>(rank);
-            nextIndex++;
         }
+
+        switch (m_suit) {
+        case CardsSuits::SUIT_CLUBS:
+            code += " Clubs";
+            break;
+        case CardsSuits::SUIT_DIAMONDS:
+            code += " Diamonds";
+            break;
+        case CardsSuits::SUIT_HEARTS:
+            code += " Hearts";
+            break;
+        case CardsSuits::SUIT_SPADES:
+            code += " Spades";
+            break;
+        default:
+            break;
+        }
+
+        return code;
     }
 
-    return deck;
-}
+    //returns a card's value
+    //const member function is a member function that guarantees it will not modify the object or call any non-const member functions (as they may modify the object)
+    int getCardValue() const {
+        if (m_rank <= CardsRanks::RANK_10) {
+            return (static_cast<int>(m_rank) + 2); //since RANK_2 is 0, adding 2 will get it's actual value
+        }
 
-//prints an entire deck
-void printDeck(const deck_type &deck) { //passed by reference
-
-    for (const Card card: deck) {
-        std::cout << printCard(card) << "\n";
-    }
-
-}
-
-//shuffles a deck
-void shuffleDeck(deck_type& deck) { //passed by reference
-    // mt is static so it only gets seeded once.
-    static std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
-
-    std::shuffle(deck.begin(), deck.end(), mt);
-}
-
-//returns a card's value
-int getCardValue(const Card& card) {
-    if (card.rank <= CardsRanks::RANK_10) { 
-        return (static_cast<int>(card.rank) + 2); //since RANK_2 is 0, adding 2 will get it's actual value
-    }
-
-    switch (card.rank) {
+        switch (m_rank) {
         case CardsRanks::RANK_JACK:
         case CardsRanks::RANK_QUEEN:
         case CardsRanks::RANK_KING:
@@ -180,8 +145,114 @@ int getCardValue(const Card& card) {
             return 11;
         default:
             return 0;
+        }
     }
-}
+
+
+};
+
+class Deck {
+public:
+    // aliases that will be used throughout the class
+    using deck_type = std::array<Card, 52>;
+    using index_type = deck_type::size_type;
+private:
+    deck_type m_deck{};
+    index_type m_cardIndex{ 0 };
+public:
+    Deck() {
+
+        //create a deck of 52 cards
+        index_type nextIndex{ 0 };
+
+        //convert enum into int to get the total number of suits and ranks
+        auto suits{ static_cast<int>(CardsSuits::MAX_SUITS) };
+        auto ranks{ static_cast<int>(CardsRanks::MAX_RANKS) };
+
+        for (int suit = 0; suit < suits; suit++) {
+            for (int rank = 0; rank < ranks; rank++) {
+                //passes a rank and suit that we casted from an integer into the constructor
+                m_deck[nextIndex] = { static_cast<CardsRanks>(rank), static_cast<CardsSuits>(suit) };
+                //next card
+                nextIndex++;
+            }
+        }
+
+    }
+
+    //prints an entire deck
+    void printDeck() { //passed by reference
+
+        for (const Card card : m_deck) {
+            std::cout << card.printCard() << "\n";
+        }
+
+    }
+
+    //shuffles a deck
+    void shuffleDeck() { //passed by reference
+
+        //reset next card index
+        m_cardIndex = 0;
+
+        // mt is static so it only gets seeded once.
+        static std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+
+        std::shuffle(m_deck.begin(), m_deck.end(), mt);
+
+        std::cout << "***********************************************The deck has been shuffled\n\n*******************************************\n\n";
+    }
+
+    //deals a card to the user 
+    const Card& dealCard() {
+        
+        if (m_cardIndex >= m_deck.size()) { //we have run out of cards to deal so we must reshuffle the deck
+            shuffleDeck();
+            return m_deck[m_cardIndex++];
+        }
+        return m_deck[m_cardIndex++]; //returns a reference to the next card in the deck
+    }
+
+
+};
+
+class Player {
+private:
+    int m_score{ 0 };
+    int m_record{ 0 };
+public:
+    //deals the player one card from the deck
+    std::string drawCard(Deck& deck) {
+
+        Card drawnCard = deck.dealCard(); //get the dealed card
+
+        m_score += drawnCard.getCardValue(); //increment the player's scire
+
+        return drawnCard.printCard(); //returns the card name
+    }
+
+    //returns true if the player's score exceeds the maximum score
+    bool isBust() const {
+        return m_score > maximumScore;
+    }
+
+    int getScore() const {
+        return m_score;
+    }
+
+    void clearScore() {
+        m_score = 0;
+    }
+
+    void incrementRecord() {
+        m_record += 1;
+    }
+
+    int getRecord() const {
+        return m_record;
+    }
+};
+
 
 //checks if a player wants to hit
 bool playerHit() {
@@ -191,69 +262,66 @@ bool playerHit() {
 
     switch (ch)
     {
-        case 'h':
-            return true;
-        case 's':
-            return false;
-        default:
-            return false;
+    case 'h':
+        return true;
+    case 's':
+        return false;
+    default:
+        return false;
     }
 }
 
+
 //Plays the player's turn
-bool playerTurn(deck_type& deck, Player &player, index_type& nextIndex) {
+bool playerTurn(Deck& deck, Player &player) {
     
     //player continues playing as long as they haven't exceeded the maximum score and they choose to hit
-    while (player.score <= maximumScore && playerHit()) { 
-        Card newCard = deck[nextIndex++];
-        player.score += getCardValue(newCard); //adds the new card's value to the player's score
-        std::cout << "You pulled a " << printCard(newCard) << ". You now have " << player.score << ".\n";
+    while (!player.isBust() && playerHit()) { 
+        std::string drawnCardName = player.drawCard(deck);
+        std::cout << "You pulled a " << drawnCardName << ". You now have " << player.getScore() << ".\n";
     }
-    return player.score > maximumScore; //returns true if the player has busted
+    return player.isBust(); //returns true if the player has busted
 }
 
 //Plays the dealer's turn
-bool dealerTurn(deck_type& deck, Player& dealer, index_type &nextIndex) {
-    while (dealer.score < minimumDealerScore) { //dealer plays as long as their score hasn't surpassed their minimum score requirement
-        Card newCard = deck[nextIndex++];
-        dealer.score += getCardValue(newCard); //adds the new card's value to the dealer's score
-        std::cout << "The dealer pulled a " << printCard(newCard) << ". He now has " << dealer.score << ".\n";
+bool dealerTurn(Deck& deck, Player& dealer) {
+    while (dealer.getScore() < minimumDealerScore) { //dealer plays as long as their score hasn't surpassed their minimum score requirement
+        std::string drawnCardName = dealer.drawCard(deck);
+        std::cout << "The dealer pulled a " << drawnCardName << ". He now has " << dealer.getScore() << ".\n";
     }
 
-    return dealer.score > maximumScore; //returns true if the dealer has busted
+    return dealer.isBust(); //returns true if the dealer has busted
 }
 
 //Main function for the blackjack game
-bool playBlackjack(deck_type& deck, index_type& nextIndex) {
+bool playBlackjack(Deck& deck, Player& player, Player& dealer) {
 
-    //reshuffles the deck and resets the card's next index if the game is running out of cards
-    if (nextIndex >= shuffleMinimum) {
-        shuffleDeck(deck);
-        nextIndex = 0;
-    }
+    //clear player's scores
+    player.clearScore();
+    dealer.clearScore();
 
     //gives the player and dealer their cards
-    Player dealer{ getCardValue(deck[nextIndex++]) };
+    dealer.drawCard(deck);
 
-    Player player{ getCardValue(deck[nextIndex]) + getCardValue(deck[nextIndex +1]) };
-    nextIndex += 2;
+    player.drawCard(deck);
+    player.drawCard(deck);
 
-    std::cout << "The dealer has: " << dealer.score << ".\n";
-    std::cout << "You have: " << player.score << ".\n";
+    std::cout << "The dealer has: " << dealer.getScore() << ".\n";
+    std::cout << "You have: " << player.getScore() << ".\n";
 
     //player goes first
-    if (playerTurn(deck, player, nextIndex)) {
+    if (playerTurn(deck, player)) {
         return false; //player has busted
     }
 
     //dealer goes next
-    if (dealerTurn(deck, dealer, nextIndex))
+    if (dealerTurn(deck, dealer))
     {
         return true; //dealer has busted
     }
     
     //whoever has the higher score wins
-    return player.score > dealer.score;
+    return player.getScore() > dealer.getScore();
 }
 
 //Returns true if the user chooses to play again
@@ -275,19 +343,28 @@ bool playAgain() {
 
 int main()
 {
-    //first start by creating and shuffling the deck
-    deck_type mainDeck = createDeck();
-    shuffleDeck(mainDeck);
-    index_type nextIndex = 0;
+    Deck deck{};
+    std::cout << "Entire shuffled deck: " << "\n";
+    deck.shuffleDeck();
+    deck.printDeck();
+
+    std::cout << "\n\n===========================================================\n\nThe game has started!\n\n";
+
+    //players
+    Player player{};
+    Player dealer{};
 
     do {
-        if (playBlackjack(mainDeck, nextIndex)) { 
-            std::cout << "You win!" << "\n"; //player has won
+        if (playBlackjack(deck, player, dealer)) { 
+            player.incrementRecord();
+            std::cout << "\nYou win!" << "\n"; //player has won
         }
         else {
-            std::cout << "You lose!" << "\n"; //player has lost
+            dealer.incrementRecord();
+            std::cout << "\nYou lose!" << "\n"; //player has lost
         }
-        std::cout << "\n" << "=============================================" << "\n";
+        std::cout << "You record is now " << player.getRecord() << " - " << dealer.getRecord();
+        std::cout << "\n" << "=============================================" << "\n\n";
     } while (true);
 
     return 0;
